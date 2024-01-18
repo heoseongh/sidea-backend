f5769f68bb6ca8a593c3367a3f1e6fc8b7aef6a6
heoseongh <shheo0807@gmail.com>
2024-01-19 06:08:05 +0900

    테스트 컨트롤러 수정

--- app/src/main/java/com/example/sideA/controller/TestController.java
+++ app/src/main/java/com/example/sideA/controller/TestController.java
@@ -3,10 +3,12 @@ package com.example.sideA.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.sideA.service.TestService;
 
+@RequestMapping("/api")
 @RestController
 public class TestController {
 	
@@ -15,24 +17,25 @@ public class TestController {
 
     @GetMapping("/hello")
     public String Hello () {
-    	
-    	
+
         return "<h1>hello World !!!</h1>";
     }
     
     @GetMapping("/DbTest")
-    public void DbTest () {
-    	
+    public String DbTest () {
+
+    	String test = "";
+
     	try {
-			String test = testService.getTestDb();
-			
+			test = testService.getTestDb();
+
 			System.out.println("!!!!"+test+"!!!!");
-			
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	
-        
+
+    	return test;
     }
 }
